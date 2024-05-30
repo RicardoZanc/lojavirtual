758eae26d32df3426c48d3387c1e6160074c3eca
Ricardo Zancan <ricardozancanc@gmail.com>
2024-05-30 19:54:13 -0300

    instalação do breeze

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/products', [ProductsController::class, 'index']);
-Route::get('/products/new', [ProductsController::class, 'create']);
-Route::post('/products/new', [ProductsController::class, 'store']);
-Route::get('/products/update/{id}', [ProductsController::class, 'edit']);
-Route::post('/products/update/', [ProductsController::class, 'update']);
-Route::get('/products/delete/{id}', [ProductsController::class, 'destroy']);
-Route::view('/dashboard', 'dashboard');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
