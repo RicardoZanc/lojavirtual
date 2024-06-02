9024f434a9018f8b5da6e991e1374f898e0be9f6
Ricardo <ricardozancanc@gmail.com>
2024-06-02 12:18:43 -0300

    readded products controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/products', [ProductsController::class, 'index']);
+Route::get('/products/new', [ProductsController::class, 'create']);
+Route::post('/products/new', [ProductsController::class, 'store']);
+Route::get('/products/update/{id}', [ProductsController::class, 'edit']);
+Route::post('/products/update/', [ProductsController::class, 'update']);
+Route::get('/products/delete/{id}', [ProductsController::class, 'destroy']);
+Route::view('/dashboard', 'dashboard');
 
 require __DIR__.'/auth.php';
