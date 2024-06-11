2a6282e33542ad70bf138fa581912ba1d6d46071
Ricardo Zancan <ricardozancanc@gmail.com>
2024-06-10 23:42:50 -0300

    starting making better type view

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -1,28 +1,29 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Produtos</title>
-</head>
-<body>
-    <h3 class="text-center">Lista de Produtos</h3>
+<x-app-layout>
     @if ($message = Session::get('success'))
     <p>{{ $message }}</p>
     @endif
     <a href="{{url('types/new')}}">
         <button>Adicionar</button>
     </a>
-    <ul>
-        @foreach($types as $type)
-        <li>
-            {{$type['name']}} {{$type['price']}}
-            <a href="{{ url('types/update', ['id' => $type->id]) }}">Editar</a>
-            <a href="{{ url('/types/delete', ['id' => $type->id]) }}">Deletar</a>
-        </li>
-        @endforeach
-    </ul>
+    <div class="card border  w-2/3 shadow-lg m-4">
+            <div class="card-header bg-gray-800 p-4">
+            <h2 class="text-xl text-white font-bold">Tipos</h2>
+            </div>
+            <div class="card-body conatiner grid grid-cols-4">
+
+            @foreach($types as $type)
+                    <div class="col-span-2">
+                    <h3 class="text-lg font-bold px-5">{{$type['name']}}</h3>
+                </div>
+                <div>
+                    <a href="{{ url('types/update', ['id' => $type->id]) }}">Editar</a>
+                </div>
+                <div>
+                    <a href="{{ url('/types/delete', ['id' => $type->id]) }}">Deletar</a>
+                </div>
+            @endforeach
+            </div>
+    </div>       
 
     <a href="{{ url('/dashboard') }}">Voltar</a>
-</body>
-</html>
+</x-app-layout>
